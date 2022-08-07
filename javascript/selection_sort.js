82045de65c289fdfbc196377895b4f86e47c7e63
Phillup Zukowksi <phillup.zukowski@gmail.com>
2022-08-07 16:42:31 -0400

    commit tree 85b59a1f46a8e034b51cdf31a0046f9111841ba7

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,18 @@
 function selectionSort(arr) {
-  // type your code here
+  // create an empty array to hold sorted elements
+  let sorted = []
+  // loop while the length of the array is greater than 0 
+  while (arr.length > 0) {
+    // find minimum value of the array
+    const min = Math.min(...arr)
+    // find the index of that value
+    const i = arr.indexOf(min)
+    // push the minimum value into sorted array
+    sorted.push(min)
+    // remove the minimum value from input array 
+    arr.splice(i, 1)
+  }
+  return sorted
 }
 
 if (require.main === module) {
@@ -15,6 +28,17 @@ if (require.main === module) {
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
+  const start = performance.now()
+  
+  for (let i = 0; i > 1000; ++i) {
+    selectedSort([1, 2, 3])
+    selectionSort(longInput)
+  }
+
+  const avgRunTime = (performance.now() - start) / 2000
+
+  console.log(avgRunTime)
+
 }
 
 module.exports = selectionSort;
